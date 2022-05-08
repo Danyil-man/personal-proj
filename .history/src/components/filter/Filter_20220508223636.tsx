27cff057e5a2f7ea838b45a2e6de83d6567cc422
Danyil <demyanyuk76@gmail.com>
2022-05-08 22:42:42 +0300

    added some general text consts

--- .history/src/components/filter/Filter_20220508223636.tsx
+++ .history/src/components/filter/Filter_20220508223636.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { sortBooksBy } from '../../consts/filter';
 import { AppStateType } from '../../store/redux/store';
