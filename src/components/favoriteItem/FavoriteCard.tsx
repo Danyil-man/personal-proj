cec74b16c72018c59946e7d7ad96691834e33cd4
Danyil <demyanyuk76@gmail.com>
2022-05-08 22:05:45 +0300

    rewrote all text into consts

--- src/components/favoriteItem/FavoriteCard.tsx
+++ src/components/favoriteItem/FavoriteCard.tsx
@@ -6,20 +6,15 @@ import { addBookCart } from '../../store/redux/reducers/cartReducer';
 import { removeFavorite } from '../../store/redux/reducers/favoriteReducer';
 import { AppStateType } from '../../store/redux/store';
 import { FavoriteBookType } from '../../types/generalTypes';
+import { author, genre, price } from '../../consts/bookInfo';
+import { uah } from '../../consts/currency';
+import { genreObj } from '../../consts/genre';
 
 type FavoriteType = {
     favorite: FavoriteBookType
     favoriteId: number
 }
 
-const obj: any = {
-    1: 'Фантастика',
-    2: 'Історія',
-    3: 'Наукова література',
-    4: 'Роман',
-    5: 'Детектив',
-    6: 'Бізнес',
-};
 const FavoriteCard: FC<FavoriteType> = ({ favorite, favoriteId }) => {
     const dispatch = useDispatch()
     const { id } = useSelector((state: AppStateType) => state.user.user)
@@ -37,12 +32,12 @@ const FavoriteCard: FC<FavoriteType> = ({ favorite, favoriteId }) => {
             </div>
             <div className={style.infoBlock}>
                 <h6>{favorite.name}</h6>
-                <p>Автор: {favorite.author}</p>
-                <p>Жанр: {obj[favorite.genreId]}</p>
+                <p>{author} {favorite.author}</p>
+                <p>{genre} {genreObj[favorite.genreId]}</p>
             </div>
 
             <div className={style.interactBlock}>
-                <p className={style.price}>Ціна: {favorite.price}грн.</p>
+                <p className={style.price}>{price} {favorite.price}{uah}</p>
                 <div className='d-flex'>
                     <Heart onClick={() => deleteFavorite(favoriteId)} color='red' className={style.icon} />
                     <Cart2 onClick={() => addBookToCart(favorite.id)} className={style.icon} />
